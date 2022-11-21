1c9885bda7967c810cfb1d6a3efc01d656e92bc7
泠羽空 <114849829+SkyOAO@users.noreply.github.com>
2022-11-21 23:10:12 +0800

    C语言实现不创建临时变量求字符串长度

--- 不创建临时变量求字符串长度/不创建临时变量求字符串长度/test.c
+++ 不创建临时变量求字符串长度/不创建临时变量求字符串长度/test.c
@@ -1,11 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-int my_strlen(char* str)
+int my_strlen(char *str)
 {
 	if (*str != '\0')//解引用用来找到字符串hello world,\0不算做数组长度的组成成分
 		return 1 + my_strlen(str + 1);//数组是指针，指针+1会向后读字符串
 	else
-		return 0;
+		return 0;//用来停下函数的调用（递归）
 }
 int main()
 {
